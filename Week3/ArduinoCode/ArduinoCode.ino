25f7fbb2a2860e87c847ace9a4743d77e0e6a647
DevasNAI <A01245418@itesm.mx>
2023-03-26 18:17:20 -0600

    Changed Arduino Comments

--- Week3/ArduinoCode/ArduinoCode.ino
+++ Week3/ArduinoCode/ArduinoCode.ino
@@ -1,38 +1,57 @@
-//Include the necessary libraries
+//  Include the necessary libraries
 #include <ros.h> //Library to use ROS commands
 #include <std_msgs/Float32.h> //Library to use Float32 type data
 
-ros::NodeHandle nh; //This command initialize the node
+//  This command initialize the node
+ros::NodeHandle nh; 
 
-//We define the pins of the Arduino and save them in the variables
-int enable = 2; //In the pin 2 we send the PWM value
-//This pins define if the motor will go clockwise or or counterclockwise
-int in1 = 3; //If we send HIGH it will go clockwise
-int in2 = 4; //If we send High it will go counterclockwise
+//  Arduino Pin definition
+//  PWM Pin
+int enable = 2;
+//  HIGH and LOW Motor pins
+int in1 = 3;
+int in2 = 4;
 
-//We define the variable PWM where we will be storing the data received from the ROS node
+//  Variable that will store /set_point  ROS Topic's value
 float PWM = 0;
 
 //In this function we receive the data of the ROS node and calculate how the motor should move
-void message( const std_msgs::Float32& PWM1){ //We receive the topic in the PWM1 variable
-  PWM = PWM1.data; //We store the topic in the PWM variable
-    //We define how the code will normally start
+/**
+ * @brief Receives a message from a ROS Topic (a PWM signal) and defines motor behavior depending on its value.
+ * If the message is negative, the motor direction is inverted. The motor will spin at the message's value.
+ * 
+ * @param PWM1 ROS /set_point value
+ */
+void message( const std_msgs::Float32& PWM1)
+{
+  //  Argument is stored in PWM Variable
+  PWM = PWM1.data; 
+  //  Motor initial behavior
   digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW);
-  if (PWM > 255){  //If the PWM value is greater than 255 (the max allowed) we only send 255 to the motor
+
+  //  Bounds the motor's maximum operation value
+  if (PWM > 255){  
     analogWrite(enable, 255);
   }
-  else if (PWM<=0){  //If the PWM is less than or equal than 0 we invert movement of the motor
+  //  If the PWM is less than or equal than 0, the motor is inverted
+  else if (PWM<=0)
+  {  
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
-    analogWrite(enable, -1*PWM1.data); // Since the PWM is negative we can't send negative values we multiply it by -1
+    //  The PWM signal can't be negative, so it is converted back to positive
+    analogWrite(enable, -1*PWM1.data);
   }
-  else if (PWM>0 and PWM<255){ //If PWM is greater than 0 and less than 255 it works normally
+  //  If PWM is in the range of 0 and 255, it behaves normally
+  else if (PWM>0 && PWM<255)
+  { 
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     analogWrite(enable, PWM1.data);
   }
-  else if (PWM<-255){  // If PWM is less than -255 we invert the movement and send a constant 255
+  // If PWM is less than -255 we invert the movement and send the maximum PWM Value
+  else if (PWM<-255)
+  {  
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     analogWrite(enable, 255);
@@ -43,21 +62,22 @@ void message( const std_msgs::Float32& PWM1){ //We receive the topic in the PWM1
 // With this we subscribe to the ROS topic and send the data to the message function
 ros::Subscriber<std_msgs::Float32> sub("/set_point", &message);
 
-// Here the code is initialize and defined
+// Arduino pin setup and Node initialization
 void setup() {
-  // put your setup code here, to run once:
+  //  H-Bridge pin setup
   pinMode(enable, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
 
-  // We initialize the node and the subscribe
+  //  Motor node initialization
   nh.initNode();
+  //  Node subscription to topic /set_point
   nh.subscribe(sub);
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Runs the code
   nh.spinOnce();
   
   delay(1);
